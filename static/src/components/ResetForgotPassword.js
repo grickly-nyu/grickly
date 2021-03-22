095b90c1b42266aec82813267476efa5782fbdb6
HengningZhang <hz1704@nyu.edu>
2021-03-21 22:59:29 -0400

    fixed reset-forgot-password

--- static/src/components/ResetForgotPassword.js
+++ static/src/components/ResetForgotPassword.js
@@ -5,7 +5,7 @@ import * as actionCreators from '../actions/auth';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import { validate_email, reset_password } from '../utils/http_functions';
+import {validate_email, reset_password} from '../utils/http_functions';
 import { browserHistory } from 'react-router';
 
 function mapStateToProps(state) {
@@ -18,7 +18,6 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch);
 }
-
 const style = {
     marginTop: 150,
     paddingTop: 40,
@@ -40,36 +39,28 @@ const titleStyle = {
 
 @connect(mapStateToProps, mapDispatchToProps)
 
-export default class ResetForgotPassword extends React.Component {
+export default class ResetForgotPassword extends React.Component { // eslint-disable-line react/prefer-stateless-function
     constructor(props) {
         super(props);
-        this.state = {
-            new_password: null,
-            re_enter: null,
-            verified: false,
-            hash: null,
-        };
-    }
-
-    componentDidMount() {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const new_hash = urlParams.get("hash");
-        this.setState({
-            hash: new_hash,
-        })
-        validate_email(new_hash).then(res => {
-            if(res.data.result) {
-                this.setState({
-                    verified: true,
-                });
+        this.state = {
+            new_password:null,
+            re_enter:null,
+            verified:false,
+            hash:urlParams.get('hash'),
+        };
+        validate_email(this.state.hash).then(response=>{
+            if(response.data.result){
+                this.setState({verified:true});
             }
         })
     }
-
+    
+    
     handleMessageSubmit(){
         if (this.state.new_password && this.state.re_enter) {
-            if(this.state.new_password != this.state.re_enter){
+            if(this.state.new_password!=this.state.re_enter){
                 alert("Conflicts between new password and re_enter, please enter the same password.");
             }
             else{
@@ -87,7 +78,9 @@ export default class ResetForgotPassword extends React.Component {
         }
         else{
             alert('Input must not be empty');
-        } 
+        }
+
+        
       }
     
     _handleKeyPress(e) {
@@ -95,56 +88,57 @@ export default class ResetForgotPassword extends React.Component {
             this.handleMessageSubmit();
         }
     }
-
     changeValue(e, type) {
         const value = e.target.value;
         const next_state = {};
         next_state[type] = value;
+        
     }
 
     render() {
+        
         return (
-            <div className="container">
-                <Paper style={style}>
-                    <div>
-                    {this.state.verified?
+            <div className = "row">
+                <div className="col-md-3  col-md-offset-1">
+                    <Paper style={style}>
+                        <div className="text-center">
+                        {this.state.verified?
                         <div>
-                            <h2 style={titleStyle}>Reset my password</h2>
                             <TextField
-                                underlineFocusStyle={{borderColor: "#FFB11B"}}
-                                floatingLabelFocusStyle={{color: "#FFB11B"}}
-                                floatingLabelText="New password"
-                                type="content"
-                                errorText={null}
-                                onChange={(e) => this.setState({new_password: e.target.value})}
+                            underlineFocusStyle={{borderColor: "#FFB11B"}}
+                            floatingLabelFocusStyle={{color: "#FFB11B"}}
+                            floatingLabelText="New password"
+                            type="content"
+                            errorText={null}
+                            onChange={(e) => this.setState({new_password: e.target.value})}
                             />
-                            <br />
                             <TextField
-                                underlineFocusStyle={{borderColor: "#FFB11B"}}
-                                floatingLabelFocusStyle={{color: "#FFB11B"}}
-                                floatingLabelText="Re-enter your new password"
-                                type="content"
-                                errorText={null}
-                                onChange={(e) => this.setState({re_enter: e.target.value})}
+                            underlineFocusStyle={{borderColor: "#FFB11B"}}
+                            floatingLabelFocusStyle={{color: "#FFB11B"}}
+                            floatingLabelText="Re-enter your new password"
+                            type="content"
+                            errorText={null}
+                            onChange={(e) => this.setState({re_enter: e.target.value})}
                             />
-                            <br />
                             <RaisedButton
                                 style={{ marginTop: 50 }}
                                 label="Confirm change of password"
-                                labelStyle="#FFB11B"
                                 onClick={() => this.handleMessageSubmit()}
                             />
                         </div>
                         :
                         <div>
-                            <p style={titleStyle}>
-                                INVALID LINK, TRY AGAIN OR CONTACT CUSTOMER SERVICE :)
-                            </p>
+                            INVALID LINK, TRY AGAIN OR CONTACT CUSTOMER SERVICE:)
                         </div>
                     }
-                    </div>
-                </Paper>
+                        </div>
+                    </Paper>
+                </div>
             </div>
         );
+        
+        
+        
+
     }
 }
