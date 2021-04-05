1616aa3bc7b4fd3474814eb9ce9d8e33809fdb60
KevinSirius <kevinsiriusyang@icloud.com>
2021-04-04 21:51:43 -0400

    delete sql URI and replace with travis secret

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,7 +3,6 @@
 
 
 set -xe
-export DATABASE_URL="mysql://doadmin:rtrjvssd4s8bpp14@grickly-nyu-do-user-8731753-0.b.db.ondigitalocean.com:25060/grickly"
 update_all() {
     pip3 install -r ./requirements.txt
     cd static
