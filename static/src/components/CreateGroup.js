276cbb5bb9dc7aed016d4956c17c6e8c52afd666
Sicong Liu <sicong.liu.simon@outlook.com>
2021-04-22 20:17:06 -0400

    Split create event (#28)
    This PR splits createEvent from createGroup page. Creating an event is optional and can be skipped if the group creator does not want an event. The new file added is CreateEvent.js.

--- static/src/components/CreateGroup.js
+++ static/src/components/CreateGroup.js
@@ -8,11 +8,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
-import DatePicker from 'material-ui/DatePicker';
-import TimePicker from 'material-ui/TimePicker';
 
 import * as actionCreators from '../actions/auth';
-import { create_event, create_group} from '../utils/http_functions';
+import {create_group} from '../utils/http_functions';
 
 function mapStateToProps(state) {
     return {
@@ -25,7 +23,6 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch);
 }
 
-const moment = require('moment');
 const style = {
     marginTop: 150,
     paddingTop: 25,
@@ -61,19 +58,12 @@ const styles = {
 export default class CreateGroup extends React.Component {
     constructor(props) {
         super(props);
-        const redirectRoute = '/chatroom';
+        const redirectRoute = '/createevent';
         this.state = {
             name: '',
             tag: '',
             redirectTo: redirectRoute,
             disabled: true,
-            event_name: '',
-            location: '',
-            start_date: null,
-            start_time: null,
-            end_date: null,
-            end_time: null,
-            description: '',
         };
     }
 
@@ -111,12 +101,6 @@ export default class CreateGroup extends React.Component {
             this.isDisabled();
         });
     }
-    
-    handleDatetimeChange(event, value, type){
-        const next_state = {};
-        next_state[type] = value;
-        this.setState(next_state)
-    }
 
     _handleKeyPress(e) {
         if (e.key === 'Enter') {
@@ -125,35 +109,9 @@ export default class CreateGroup extends React.Component {
             }
         }
     }
-
-    mergeDatetime(){
-        let momentTime = moment(this.state.start_time);
-        let momentDate = moment(this.state.start_date);
-        let start_time = moment({
-            year: momentDate.year(),
-            month: momentDate.month(),
-            day: momentDate.date(),
-            hour: momentTime.hours(),
-            minute: momentTime.minutes()
-        });
-        momentTime = moment(this.state.end_time);
-        momentDate = moment(this.state.end_date);
-        let end_time = moment({
-            year: momentDate.year(),
-            month: momentDate.month(),
-            day: momentDate.date(),
-            hour: momentTime.hours(),
-            minute: momentTime.minutes()
-        });
-        this.setState({
-            start_time: start_time,
-            end_time: end_time
-        }, ()=>{console.log(this.state.start_time, this.state.end_time)})
-    }
     
     createGroup(e) {
         e.preventDefault();
-        this.mergeDatetime();
         create_group(this.state.name, this.state.tag).then(response =>{
             var path = {
                 pathname: this.state.redirectTo,
@@ -162,13 +120,6 @@ export default class CreateGroup extends React.Component {
                     name: this.state.name,
                 },
             }
-            create_event(
-                response.data.results, 
-                this.state.event_name, 
-                this.state.location, 
-                this.state.start_time, 
-                this.state.end_time, 
-                this.state.description)
             this.dispatchNewRoute(path)
         });
         
@@ -207,70 +158,6 @@ export default class CreateGroup extends React.Component {
                             <MenuItem value={"Study"} primaryText="Study" />
                             <MenuItem value={"Party"} primaryText="Party" />
                         </DropDownMenu>
-                        <br />
-                        <TextField
-                            inputStyle = {styles.vStyle}
-                            hintText="Event title"
-                            hintStyle={styles.pStyle}
-                            floatingLabelText="Event title"
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            underlineFocusStyle={styles.underlineFocusStyle}
-                            type="event_name"
-                            errorText={null}
-                            onChange={(e) => this.changeValue(e, 'event_name')}
-                        />
-                        <br />
-                        <TextField
-                            inputStyle = {styles.vStyle}
-                            hintText="Event Description"
-                            hintStyle={styles.pStyle}
-                            floatingLabelText="Event Description"
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            underlineFocusStyle={styles.underlineFocusStyle}
-                            type="description"
-                            errorText={null}
-                            onChange={(e) => this.changeValue(e, 'description')}
-                        />
-                        <br />
-                        <TextField
-                            inputStyle = {styles.vStyle}
-                            hintText="Event Location"
-                            hintStyle={styles.pStyle}
-                            floatingLabelText="Event Location"
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            underlineFocusStyle={styles.underlineFocusStyle}
-                            type="location"
-                            errorText={null}
-                            onChange={(e) => this.changeValue(e, 'location')}
-                        />
-                        <br />
-                        <DatePicker 
-                            hintText="Event Start Date" 
-                            hintStyle={styles.pStyle}
-                            inputStyle = {styles.vStyle}
-                            onChange={(event, value) => this.handleDatetimeChange(event, value, 'start_date')}
-                        />
-                        <TimePicker
-                            hintText="Event Start Time" 
-                            hintStyle={styles.pStyle}
-                            inputStyle = {styles.vStyle}
-                            onChange={(event, value) => this.handleDatetimeChange(event, value, 'start_time')}
-                        />
-                        <DatePicker
-                            hintText="Event End Date" 
-                            hintStyle={styles.pStyle}
-                            inputStyle = {styles.vStyle}
-                            onChange={(event, value) => this.handleDatetimeChange(event, value, 'end_date')}
-                        />
-                        <TimePicker
-                            hintText="Event End Time" 
-                            hintStyle={styles.pStyle}
-                            inputStyle = {styles.vStyle}
-                            onChange={(event, value) => this.handleDatetimeChange(event, value, 'end_time')}
-                        />
                         <br />
                         <RaisedButton
                             disabled={this.state.disabled}
